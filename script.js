26aef142c0d6def5f95bd347f66da5a03e589fa3
urielsuarezbaltazar <urielsuarezbaltazar@gmail.com>
2026-02-14 00:57:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let musicStarted=false;
 let noClicked=false;
 
+generateStars();
+createGalaxy();
+
 function answer(val){
   if(!val){
     if(!noClicked){
@@ -46,25 +49,69 @@ function nextScene(){
   }
 }
 
+/* ===== FONDO ===== */
+
+function generateStars(){
+  const starsContainer=document.querySelector(".stars");
+  for(let i=0;i<120;i++){
+    const star=document.createElement("div");
+    star.className="star";
+    star.style.top=Math.random()*100+"%";
+    star.style.left=Math.random()*100+"%";
+    star.style.animationDuration=(1+Math.random()*3)+"s";
+    starsContainer.appendChild(star);
+  }
+}
+
+function createGalaxy(){
+  const galaxy=document.createElement("div");
+  galaxy.className="galaxy";
+  document.body.appendChild(galaxy);
+}
+
+/* ===== ESTRELLA FUGAZ ===== */
+
 function launchStar(){
   const star=document.createElement("div");
   star.className="shooting-star";
+  star.textContent="🌠";
 
-  star.style.left=Math.random()*window.innerWidth+"px";
-  star.style.top="-10px";
+  const trail=document.createElement("div");
+  trail.className="trail";
 
+  star.style.left="100%";
+  star.style.top="0%";
+
+  document.body.appendChild(trail);
   document.body.appendChild(star);
 
   star.animate([
     {transform:"translate(0,0)"},
-    {transform:"translate(-400px,600px)"}
+    {transform:"translate(-50vw,50vh)"}
+  ],{duration:2000,easing:"ease-in"});
+
+  trail.animate([
+    {transform:"translate(100vw,0) rotate(45deg)"},
+    {transform:"translate(50vw,50vh) rotate(45deg)"}
   ],{duration:2000,easing:"ease-in"});
 
   setTimeout(()=>{
-    document.getElementById("impactHeart").style.opacity=1;
-    document.getElementById("astronaut").style.opacity=1;
+    impact();
   },2000);
 }
+
+function impact(){
+  const heart=document.getElementById("impactHeart");
+  heart.classList.add("show");
+  heart.style.position="absolute";
+  heart.style.top="50%";
+  heart.style.left="50%";
+  heart.style.transform="translate(-50%,-50%) scale(1.2)";
+
+  document.getElementById("astronaut").style.opacity=1;
+}
+
+/* ===== CORAZONES ===== */
 
 function createFloatingHearts(){
   const container=document.getElementById("floatingHearts");
